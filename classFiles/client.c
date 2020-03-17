b0eee96bcdf156ceb62a71cf3f100e57fcaf295f
Ari Roffe <aroffe@mail.yu.edu>
2020-03-17 12:23:14 -0500

    Add files via upload

--- classFiles/client.c
+++ classFiles/client.c
@@ -22,19 +22,20 @@
 #define FIFO 0
 #define CONCUR 1
 
-typedef struct {
+typedef struct
+{
   size_t thread_num;
   pthread_mutex_t work_mutex;
   pthread_cond_t c_cond; // P/C condition variables
   pthread_cond_t p_cond;
-} tpool_t;//KRAZY FOR KELLY
+
+  char* host;
+  char* port;
+} tpool_t; //KRAZY FOR KELLY
 
 typedef void *(worker_fn)(void *);
-static tpool_t the_pool;
-static char* host;
-static char* port;
-static int threads;
-static char* filename;
+//static int threads;
+static char *filename;
 int i;
 static pthread_barrier_t barrier;
 
@@ -61,22 +62,25 @@ void tpool_init_client(tpool_t *tm, size_t num_threads, worker_fn worker)
 
   tm->thread_num = num_threads;
 
-  for(i = 0; i < num_threads; i++){
+  for (i = 0; i < num_threads; i++)
+  {
     j = i + 1;
     pthread_create(&thread, NULL, worker, (void *)j);
     pthread_detach(thread);
   }
 }
 
-  // Get host information (used to establishConnection)
-  struct addrinfo *getHostInfo(char *host, char *port){
+// Get host information (used to establishConnection)
+struct addrinfo *getHostInfo(char *host, char *port)
+{
   int r;
   struct addrinfo hints, *getaddrinfo_res;
   // Setup hints
   memset(&hints, 0, sizeof(hints));
   hints.ai_family = AF_INET;
   hints.ai_socktype = SOCK_STREAM;
-  if ((r = getaddrinfo(host, port, &hints, &getaddrinfo_res))) {
+  if ((r = getaddrinfo(host, port, &hints, &getaddrinfo_res)))
+  {
     fprintf(stderr, "[getHostInfo:21:getaddrinfo] %s\n", gai_strerror(r));
     return NULL;
   }
@@ -85,20 +89,25 @@ void tpool_init_client(tpool_t *tm, size_t num_threads, worker_fn worker)
 }
 
 // Establish connection with host
-int establishConnection(struct addrinfo *info) {
-  if (info == NULL) return -1;
+int establishConnection(struct addrinfo *info)
+{
+  if (info == NULL)
+    return -1;
 
   int clientfd;
-  for (;info != NULL; info = info->ai_next) {
+  for (; info != NULL; info = info->ai_next)
+  {
     if ((clientfd = socket(info->ai_family,
                            info->ai_socktype,
-                           info->ai_protocol)) < 0) {
+                           info->ai_protocol)) < 0)
+    {
       perror("[establishConnection:35:socket]");
       continue;
     }
 
     //clientfd is file descriptor that refers to the endpoint created by the socket method
-    if (connect(clientfd, info->ai_addr, info->ai_addrlen) < 0) {
+    if (connect(clientfd, info->ai_addr, info->ai_addrlen) < 0)
+    {
       close(clientfd);
       perror("[establishConnection:42:connect]");
       continue;
@@ -113,24 +122,28 @@ int establishConnection(struct addrinfo *info) {
 }
 
 // Send GET request
-void GET(int clientfd, char *path) {
+void GET(int clientfd, char *path)
+{
   char req[1000] = {0};
   sprintf(req, "GET %s HTTP/1.0\r\n\r\n", path);
   send(clientfd, req, strlen(req), 0);
 }
 
-static void *client_worker_fifo(void * arg){
+static void *client_worker_fifo(void *arg)
+{
   char buf[BUF_SIZE];
   //each thread sends a reuest to the server
   /*1. request the same file*/
-  while(1){
+  while (1)
+  {
     pthread_mutex_lock(&(client_pool->work_mutex));
     //CONNECT
-    int clientfd = establishConnection(getHostInfo(host, port));
-    if (clientfd == -1){
+    int clientfd = establishConnection(getHostInfo(client_pool->host, client_pool->port));
+    if (clientfd == -1)
+    {
       fprintf(stderr,
               "[main:73] Failed to connect to: %s:%s%s \n",
-              host, port, filename);
+              client_pool->host, client_pool->port, filename);
       exit(3);
     }
     //GET request
@@ -138,7 +151,8 @@ static void *client_worker_fifo(void * arg){
     //RELEASES TO ANOTHER THREAD
     pthread_mutex_unlock(&(client_pool->work_mutex));
     //RECEIVE
-    while (recv(clientfd, buf, BUF_SIZE, 0) > 0) {
+    while (recv(clientfd, buf, BUF_SIZE, 0) > 0)
+    {
       fputs(buf, stdout);
       memset(buf, 0, BUF_SIZE);
     }
@@ -148,19 +162,23 @@ static void *client_worker_fifo(void * arg){
   }
 }
 
-static void *client_worker_concur(void* arg){
+static void *client_worker_concur(void *arg)
+{
   char buf[BUF_SIZE];
-  while(1){
-    int clientfd = establishConnection(getHostInfo(host, port));
-    if (clientfd == -1){
+  while (1)
+  {
+    int clientfd = establishConnection(getHostInfo(client_pool->host, client_pool->port));
+    if (clientfd == -1)
+    {
       fprintf(stderr,
               "[main:73] Failed to connect to: %s:%s%s \n",
-              host, port, filename);
+              client_pool->host, client_pool->port, filename);
       exit(3);
     }
 
     GET(clientfd, filename);
-    while (recv(clientfd, buf, BUF_SIZE, 0) > 0) {
+    while (recv(clientfd, buf, BUF_SIZE, 0) > 0)
+    {
       fputs(buf, stdout);
       memset(buf, 0, BUF_SIZE);
     }
@@ -170,40 +188,43 @@ static void *client_worker_concur(void* arg){
   }
 }
 
-    int main(int argc, char **argv){
+int main(int argc, char **argv)
+{
   int schedalg;
-  int clientfd;
+  //int clientfd;
 
-  int port, threads;
-  struct sockaddr_in server;//socket information about the server
-  struct sockaddr_in client;//socket information about
+  //int port, threads;
+  //struct sockaddr_in server; //socket information about the server
+  //struct sockaddr_in client; //socket information about
 
-  int clientLen;
-  size_t i, j;//for the loop counter
-
-  if (argc != 6){
+  if (argc != 6)
+  {
     fprintf(stderr, "USAGE: %s <hostname> <port> <threads> <schedalg> <filename1> <filename2> \n", argv[0]);
     return 1;
   }
-  port = atoi(argv[2]);
-  threads = atoi(argv[3]);
+  client_pool->host = argv[2];
+  client_pool->port = argv[3];
 
   /*Initialize a thread pool*/
   //Set the schedalg:
-			if(strcmp(argv[4], "CONCUR")==0) schedalg = CONCUR;
-			else if(strcmp(argv[4], "FIFO")==0) schedalg = FIFO;
-      else ;//FIXME: invalid schedalg;
-  
-  switch (schedalg){
-    case CONCUR:
-      tpool_init_client(client_pool, threads, client_worker_concur);
-      break;
-    case FIFO:
-      tpool_init_client(client_pool, threads, client_worker_fifo);
-      break;
-    default:
-      exit(0);
-  }  
+  if (strcmp(argv[4], "CONCUR") == 0)
+    schedalg = CONCUR;
+  else if (strcmp(argv[4], "FIFO") == 0)
+    schedalg = FIFO;
+  else
+    schedalg = FIFO; //FIXME: invalid schedalg;
+
+  switch (schedalg)
+  {
+  case CONCUR:
+    tpool_init_client(client_pool, atoi(argv[3]), client_worker_concur);
+    break;
+  case FIFO:
+    tpool_init_client(client_pool, atoi(argv[3]), client_worker_fifo);
+    break;
+  default:
+    exit(0);
+  }
 
   /* The program ./client is run w/the following args
   argv[1] ==> [host] 
@@ -213,6 +234,6 @@ static void *client_worker_concur(void* arg){
   argv[5] ==> [filename1]
   argv[6] ==> [filename2]
   */
-    return 0;
+  return 0;
 
-}//end of main
+} //end of main
